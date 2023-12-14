1284a926b08d07beb2030352aaf10ac722394ba2
BomberFish <bomberfish@bomberfish.ca>
2023-12-14 08:50:54 -0500

    Initial Commit

---
+++ SwiftTop/SwiftTopApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// SwiftTopApp.swift – SwiftTop
+// created on 2023-12-14
+
+import SwiftUI
+
+@main
+struct SwiftTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
